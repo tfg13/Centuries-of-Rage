43ba130a84ed99e928fed9f6810d2ff6651901c2
tfg/MACS <tobifleig@gmail.com>
2011-01-12 19:19:17 +0100

    Erste Version von graphics/input/InteractableGameElement.java

---
+++ src/thirteenducks/cor/graphics/input/InteractableGameElement.java
@@ -0,0 +1,53 @@
+/*
+ *  Copyright 2008, 2009, 2010, 2011:
+ *   Tobias Fleig (tfg[AT]online[DOT]de),
+ *   Michael Haas (mekhar[AT]gmx[DOT]de),
+ *   Johannes Kattinger (johanneskattinger[AT]gmx[DOT]de)
+ *
+ *  - All rights reserved -
+ *
+ *
+ *  This file is part of Centuries of Rage.
+ *
+ *  Centuries of Rage is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Centuries of Rage is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Centuries of Rage.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package thirteenducks.cor.graphics.input;
+
+/**
+ * InteractableGameElements sind Spielelemente, mit denen (durch die Maus) interagiert werden kann.
+ *
+ * Dieses Interface regelt, was passiert, wenn die Maus über eine Objekt bewegt wird, wenn es angeklickt wird/ etc.
+ * Nur die Selektion selber findet nicht hier statt, dafür ist das Inputmodul selber zuständig
+ */
+public interface InteractableGameElement {
+
+    /**
+     * Die Maus befindet sich über dem InteractableGameElement.
+     */
+    public void mouseHovered();
+    /**
+     * Dieses InteractableGameElement wurde angeklickt.
+     */
+    public void mouseKlicked();
+    /**
+     * Die Maus wurde über diesem InteractableGameElement gedrückt.
+     */
+    public void mousePressed();
+    /**
+     * Die Maus wurde über diesem InteractableGameElement loggelassen.
+     */
+    public void mouseReleased();
+}
