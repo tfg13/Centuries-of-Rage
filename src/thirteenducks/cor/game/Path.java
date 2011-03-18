65a6144066f53d4e117852bbaea3300f70a2cbbb
Tulius <tobifleig@gmail.com>
2011-03-18 19:45:22 +0100

    fix: bewegungssystem soweit stabil, komischer dauernullpointer gefixt. (Zeile von übriggebliebenem Angriffscode auskommentiert)

--- src/thirteenducks/cor/game/Path.java
+++ src/thirteenducks/cor/game/Path.java
@@ -266,10 +266,11 @@ public class Path implements Pauseable, Serializable {
                         // Fertig, Bewegung stoppen
                         caster2.setMainPosition(targetPos);
                         targetPos = null;
+                        System.out.println("Stiop");
                         path = null;
                         rgi.netmap.setCollision(caster2.getMainPosition(), collision.occupied);
                         rgi.netmap.setUnitRef(caster2.getMainPosition(), caster2, caster2.getPlayerId());
-                        caster2.attackManager.moveStopped();
+                        //caster2.attackManager.moveStopped();
                         moving = false;
                         return;
                     }
