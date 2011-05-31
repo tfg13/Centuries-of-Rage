a9fe6e5308ce073c3bfd69b5fe07bee96278fa96
Tulius <tobifleig@gmail.com>
2011-05-31 13:30:27 +0200

    Neu: Der SendToEffect

---
+++ src/de/_13ducks/cor/graphics/effects/SendToEffect.java
@@ -0,0 +1,71 @@
+/*
+ *  Copyright 2008, 2009, 2010, 2011:
+ *   Tobias Fleig (tfg[AT]online[DOT]de),
+ *   Michael Haas (mekhar[AT]gmx[DOT]de),
+ *   Johannes Kattinger (johanneskattinger[AT]gmx[DOT]de)
+ *
+ *  - All rights reserved -
+ *
+ *
+ *  This file is part of Centuries of Rage.
+ *
+ *  Centuries of Rage is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Centuries of Rage is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Centuries of Rage.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package de._13ducks.cor.graphics.effects;
+
+import de._13ducks.cor.graphics.GraphicsContent;
+import de._13ducks.cor.graphics.GraphicsImage;
+import java.util.Map;
+import org.newdawn.slick.Graphics;
+
+/**
+ * Der Effekt, der angezeigt wird, wenn mit selektierten Einheiten auf den Boden rechtsgeklickt wird.
+ */
+public class SendToEffect extends SkyEffect {
+    
+    private static final int DURATION = 500;
+    
+    /**
+     * Die x-Koordinate dieses Effekts
+     */
+    private double x;
+    /**
+     * Die y-Koordinate dieses Effekts
+     */
+    private double y;
+    private long finish;
+    
+    public SendToEffect(double x, double y) {
+        this.x = x * GraphicsContent.FIELD_HALF_X + GraphicsContent.OFFSET_PRECISE_X;
+        this.y = y * GraphicsContent.FIELD_HALF_Y + GraphicsContent.OFFSET_PRECISE_Y;
+        finish = System.currentTimeMillis() + DURATION;
+    }
+
+    @Override
+    public void renderSkyEffect(Graphics g, double scrollX, double scrollY, Map<String, GraphicsImage> imgMap) {
+        imgMap.get("img/game/send.png").getImage().draw((float) (x - scrollX), (float) (y - scrollY));
+    }
+
+    @Override
+    public boolean isVisible(double scrollX, double scrollY, int resX, int resY) {
+        return scrollX <= x && x <= (scrollX + resX) && scrollY <= y && y <= (scrollY + resY);
+    }
+
+    @Override
+    public boolean isDone() {
+        return System.currentTimeMillis() >= finish;
+    }
+    
+}
