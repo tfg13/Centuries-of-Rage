02d67ebbe394af0ec40d3fac050d9810a62b4534
tulius <tobifleig@gmail.com>
2011-07-22 16:32:41 +0200

    SubSectorPathfinder: Aufbau des Graphen: Schnittpunkte werden berechnet

--- src/de/_13ducks/cor/game/server/movement/SubSectorPathfinder.java
+++ src/de/_13ducks/cor/game/server/movement/SubSectorPathfinder.java
@@ -27,6 +27,7 @@ package de._13ducks.cor.game.server.movement;
 
 import de._13ducks.cor.game.Moveable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -71,7 +72,8 @@ public class SubSectorPathfinder {
             // Mit Graph vernetzen
             for (SubSectorObstacle node : graph) {
                 if (node.inColRange(next, radius)) {
-                    
+                    // Schnittpunkte suchen
+                    SubSectorNode[] intersections = node.calcIntersections(next, radius);
                 }
             }
             // Weitere Hindernisse suchen, die jetzt relevant sind.
@@ -154,6 +156,57 @@ public class SubSectorPathfinder {
          */
         double getRadius() {
             return radius;
+        }
+
+        /**
+         * Berechnet die Schnittpunkte der Lauflinien dieses und des gegebenen
+         * Hindernisses
+         * Setzt voraus, dass es zwei Schnittpunkte gibt. Das Verhalten dieser
+         * Methode ist in anderen Fällen nicht definiert.
+         * @param next Das andere Hinderniss
+         * @param radius Der Radius (die Größe) des Movers, der noch zwischendurch passen muss.
+         * @return SubSectorNode[] mit 2 Einträgen
+         */
+        private SubSectorNode[] calcIntersections(SubSectorObstacle next, double radius) {
+            // Zuerst Mittelpunkt der Linie durch beide Schnittpunkte berechnen:
+            Vector direct = new Vector(next.x - x, next.y - y);
+            Vector z1 = direct.normalize().multiply(this.radius + radius);
+            Vector z2 = direct.normalize().multiply(direct.length() - (next.radius + radius));
+            Vector mid = direct.normalize().multiply((z1.length() + z2.length()) / 2.0);
+            // Senkrechten Vektor und seine Länge berechnen:
+            Vector ortho = new Vector(direct.y(), -direct.x());
+            ortho.normalize().multiply(Math.sqrt(((this.radius + radius) * (this.radius + radius)) - (mid.length() * mid.length())));
+            // Schnittpunkte ausrechnen:
+            SubSectorNode[] intersections = new SubSectorNode[2];
+            Vector posMid = new Vector(x + mid.x(), y + mid.y()); // Positionsvektor des Mittelpunkts
+            Vector s1 = posMid.add(ortho);
+            Vector s2 = posMid.add(ortho.getInverted());
+            intersections[0] = new SubSectorNode(s1.x(), s1.y(), this, next);
+            intersections[1] = new SubSectorNode(s2.x(), s2.y(), this, next);
+            return intersections;
+        }
+        
+    }
+    
+    private static class SubSectorNode {
+        /**
+         * Koordinaten
+         */
+        private double x;
+        /**
+         * Koordinaten
+         */
+        private double y;
+        /**
+         * Auf wessen Laufkreis liegt dieses Hinderniss?
+         */
+        private ArrayList<SubSectorObstacle> myObstacle;
+        
+        SubSectorNode(double x, double y, SubSectorObstacle... owner) {
+            this.x = x;
+            this.y = y;
+            myObstacle = new ArrayList<SubSectorObstacle>();
+            myObstacle.addAll(Arrays.asList(owner));
         }
         
     }
