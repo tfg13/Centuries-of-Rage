e3f2f2b16b4e3345c5802f2aeeefaaf065c0293a
tulius <tobifleig@gmail.com>
2011-07-22 14:38:52 +0200

    Anfang vom Ingame-Menu

--- src/de/_13ducks/cor/graphics/IngameMenu.java
+++ src/de/_13ducks/cor/graphics/IngameMenu.java
@@ -2,9 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de._13ducks.cor.graphics;
 
+import de._13ducks.cor.game.client.Client;
+import de._13ducks.cor.graphics.input.OverlayMouseListener;
 import org.newdawn.slick.Graphics;
 
 /**
@@ -13,13 +14,71 @@ import org.newdawn.slick.Graphics;
  */
 public class IngameMenu extends Overlay {
 
+    private static final int DURATION = 1000;
+    private int x1, y1, x2, y2;
+    private long startTime;
+    private boolean out = false;
+
     @Override
     public void renderOverlay(Graphics g, int fullResX, int fullResY) {
-        Renderer.drawImage("img/hud/menubutton.png", (int) (fullResX * 0.45), 0, (int) (fullResX * 0.1), (int) (fullResY * 0.03));
+        long passedTime = System.currentTimeMillis() - startTime;
+        double k = 0.1 * fullResX / 128;
+        int x = (int) ((fullResX / 2) - (207 * k));
+        int y = (int) (-(512 - 55) * k);
+        x1 = (int) (fullResX * 0.45);
+        x2 = (int) (fullResX * 0.55);
+        y1 = 0;
+        y2 = (int) (fullResX * 0.1 * 55 / 128);
+        if (passedTime < DURATION) {
+            double progress = 1 - ((Math.cos(passedTime * Math.PI / DURATION) + 1) / 2);
+           y = (int) (-(512 - 55) * k * (1 - progress));
+        }
+        Renderer.drawImage("img/hud/tilemap_hud_stoneage.png", x, y, x + (k * 350), y + (k * 512), 0, 0, 350, 511);
     }
 
     public IngameMenu() {
-	
-    }
+        Client.getInnerClient().rogGraphics.inputM.addOverlayMouseListener(new OverlayMouseListener() {
 
+            @Override
+            public int getCatch1X() {
+                return x1;
+            }
+
+            @Override
+            public int getCatch1Y() {
+                return y1;
+            }
+
+            @Override
+            public int getCatch2X() {
+                return x2;
+            }
+
+            @Override
+            public int getCatch2Y() {
+                return y2;
+            }
+
+            @Override
+            public void mouseMoved(int x, int y) {
+            }
+
+            @Override
+            public void mouseDragged(int x, int y) {
+            }
+
+            @Override
+            public void mouseWheelMoved(int change) {
+            }
+
+            @Override
+            public void mousePressed(int button, int x, int y) {
+            }
+
+            @Override
+            public void mouseReleased(int button, int x, int y) {
+                startTime = System.currentTimeMillis();
+            }
+        });
+    }
 }
